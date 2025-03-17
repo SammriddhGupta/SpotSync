4331cc5431d365d6e683bf101e44824743ec6473
SammriddhGupta <sammriddhgupta@gmail.com>
2025-03-17 21:58:32 +1100

    fix: remove base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/',
   plugins: [react()],
 })
